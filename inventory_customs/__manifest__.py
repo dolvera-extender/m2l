81782089e4041f086996f923f3a75a86bca39880
isaac <pcgi0001x@gmail.com>
2022-03-11 15:36:37 -0600

    Customización de recepciones

---
+++ inventory_customs/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Inventory Customs",
+    'version': '0.01b',
+    'author': 'Ing. Isaac Chávez Arroyo',
+    'website': 'https://isaaccv.ml',
+    "depends": ["base"],
+    "data": [
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'LGPL-3'
+}
