52e6c5d06411dca4603af45af0b5e154ca8b1a60
isaac <pcgi0001x@gmail.com>
2022-03-11 19:05:10 -0600

    Entry flag added

--- inventory_customs/models/stock_picking.py
+++ inventory_customs/models/stock_picking.py
@@ -11,8 +11,6 @@ class StockPickingCustom(models.Model):
     is_entry = fields.Boolean(string="Es entrada", compute="_check_if_is_entry", store=False)
 
     def _check_if_is_entry(self):
-        # Check if is entry, using picking_type_id.name ==
-        _log.info(" ______________ tipo de operación:: %s" % self.picking_type_id)
         is_entry = True if self.picking_type_id and self.picking_type_id.name == "Recepciones" else False
         self.is_entry = is_entry
         return is_entry
