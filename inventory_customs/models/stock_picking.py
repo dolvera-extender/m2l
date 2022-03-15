4eb14c2cb9de8669e8375b28aadaff7c25a54577
isaac_mac <ich3869@gmail.com>
2022-03-14 23:59:52 -0600

    add: multiplier fields in picking form

--- inventory_customs/models/stock_picking.py
+++ inventory_customs/models/stock_picking.py
@@ -9,11 +9,26 @@ class StockPickingCustom(models.Model):
     _inherit = "stock.picking"
 
     use_multiplier = fields.Boolean(string="Usar multiplicador", compute="_check_use_multiplier", store=False)
+    product_to_multiply = fields.Many2one('product.product', string="Producto a multiplicar")
+    product_pack_qty = fields.Integer(string="Cantidad por paquete")
+    product_av_qty = fields.Integer(string="Cantidad a empaquetar")
 
     def _check_use_multiplier(self):
         use_multiplier = True if self.picking_type_id and self.picking_type_id.use_multiplier else False
         self.use_multiplier = use_multiplier
         return use_multiplier
+
+    def multiply_product(self):
+        """
+        Method that create packages with computed number of lots for stock picking
+        :return:
+        """
+        _log.info("Usando multiplicador.")
+        pass
+
+    @api.onchange('product_to_multiply')
+    def default_product_av_qty(self):
+        pass
 
 
 class StockPickingTypeCustom(models.Model):
