48f874f062f022f7aca5b64f71cbbecece37e1ca
isaac_mac <ich3869@gmail.com>
2022-03-21 14:15:47 -0600

    Modf:: add no suggest lines

--- inventory_customs/models/stock_picking.py
+++ inventory_customs/models/stock_picking.py
@@ -27,13 +27,16 @@ class StockPickingCustom(models.Model):
         :return:
         """
         move_id_multiply = self.move_ids_without_package.filtered(lambda li: li.product_id.id == self.product_to_multiply.id)
-        _log.info("\n LINEA A MULTIPLICAR:: %s \nCon el producto:: %s" % (move_id_multiply, move_id_multiply.product_id.name))
-        
+        _log.info("\n LINEA A MULTIPLICAR:: %s \nCon el producto:: %s " % (move_id_multiply, move_id_multiply.product_id.name))
+        for line in move_id_multiply.move_line_nosuggest_ids:
+            _log.info("\n\n LOCATION:: %s  Location name: %s" % (line.location_id, line.location_id.name))
+
         qty_for_done = move_id_multiply.product_uom_qty - move_id_multiply.quantity_done
         qty_iterations = int(qty_for_done/self.product_qty_pack)
         qty_residual = qty_for_done%self.product_qty_pack
         _log.info("\n\n FALTAN %s UNIDADES DE PRODUCTO ... " % qty_for_done)
         # move_id_multiply.move_line_nosuggest_ids = ()
+        """
         moves_for_add = []
         iteracion = 0
         while True:
@@ -67,7 +70,7 @@ class StockPickingCustom(models.Model):
                 qty_residual = 0
         _log.info(" DATOS A INSERTAR::: %s " % moves_for_add)
         move_id_multiply.move_line_nosuggest_ids = moves_for_add
-
+        """
 
     # @api.onchange('product_to_multiply')
     # def default_product_av_qty(self):
