22a15dbe08f023b9b3fde297d28902ab70d0391b
IsaacChavezArroyo <Isaac>
2022-06-15 12:22:28 -0500

    package as testing

--- sale_mps/models/packages.py
+++ sale_mps/models/packages.py
@@ -126,3 +126,34 @@ class StockPickingMps(models.Model):
         _log.info("\n move lines ::: %s " % mlids)
         self.move_line_ids_without_package = mlids
 
+
+class StockMoveMPs(models.Model):
+    _inherit = "stock.move"
+
+    def _set_lot_ids(self):
+        for move in self:
+            if move.product_id.tracking != 'serial':
+                continue
+            move_lines_commands = []
+            if move.picking_type_id.show_reserved is False:
+                mls = move.move_line_nosuggest_ids
+            else:
+                mls = move.move_line_ids
+            mls = mls.filtered(lambda ml: ml.lot_id)
+            for ml in mls:
+                if ml.qty_done and ml.lot_id not in move.lot_ids:
+                    move_lines_commands.append((2, ml.id))
+            ls = move.move_line_ids.lot_id
+            for lot in move.lot_ids:
+                _log.info("\n LOTE ?? %s " % lot.name)
+                if lot not in ls:
+                    move_line_vals = self._prepare_move_line_vals(quantity=0)
+                    move_line_vals['lot_id'] = lot.id
+                    move_line_vals['lot_name'] = lot.name
+                    move_line_vals['product_uom_id'] = move.product_id.uom_id.id
+                    move_line_vals['qty_done'] = 1
+                    move_lines_commands.append((0, 0, move_line_vals))
+                else:
+                    move_line = move.move_line_ids.filtered(lambda line: line.lot_id.id == lot.id)
+                    move_line.qty_done = 1
+            move.write({'move_line_ids': move_lines_commands})
