e3cb5823c3fc2d88e99e80c62e28003e11690d7c
IsaacCA <IsaacCA>
2022-07-04 20:52:16 -0500

    report fixed

---
+++ m2l_delivery_report/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Delivery report",
+    'version': '15.01b',
+    'author': 'Ing. Isaac Chávez Arroyo',
+    'website': 'https://isaaccv.ml',
+    "depends": ["base", "stock", "sale"],
+    "data": [
+        "reports/delivery_reports.xml"
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'LGPL-3'
+}
