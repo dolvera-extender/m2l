db03d86c7a4b486086887f0ba859256087c0cc26
IsChA <ich3869@gmail.com>
2022-10-04 20:47:34 -0500

    Chgnd: some styles

--- m2l_delivery_report/product.py
+++ m2l_delivery_report/product.py
@@ -11,6 +11,6 @@ class ProductDr(models.Model):
     _inherit = "product.product"
 
     def get_product_head_text(self):
-        _log.info(" DIR DESCR:: %s " % dir(self.description))
+        # _log.info(" DIR DESCR:: %s " % dir(self.description))
         text = re.compile('<.*?>')
         return "[%s] %s" % (self.categ_id.name, re.sub(text, '', self.description))
