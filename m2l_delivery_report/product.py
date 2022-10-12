30bc46ceb897db00a900b852a3589f10960f07b4
IsChA <ich3869@gmail.com>
2022-10-11 19:05:29 -0500

    Fix: consider some products without description

--- m2l_delivery_report/product.py
+++ m2l_delivery_report/product.py
@@ -13,4 +13,4 @@ class ProductDr(models.Model):
     def get_product_head_text(self):
         # _log.info(" DIR DESCR:: %s " % dir(self.description))
         text = re.compile('<.*?>')
-        return "[%s] %s" % (self.categ_id.name, re.sub(text, '', self.description))
+        return "[%s] %s" % (self.categ_id.name, re.sub(text, '', self.description or " "))
