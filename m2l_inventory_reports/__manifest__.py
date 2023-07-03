0581d0fecf7fd35034ddc28c6e5bd07038404ebf
IsaacThkbk <ich3869@gmail.com>
2023-07-03 13:31:08 -0600

    Base development addon

---
+++ m2l_inventory_reports/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inventory user reports',
+    'version': '0.1b',
+    'summary': 'Reportes de movimientos de inventario y productos',
+    "author": "Ing. Isaac Chávez Arroyo",
+    'description': "",
+    'website': 'https://isaaccv.ml',
+    'depends': ['base', 'stock'],
+    'category': 'Inventory/Inventory',
+    'sequence': 88,
+    'data': [
+        'security/groups.xml',
+        'views/views.xml',
+        'views/actions.xml',
+        'views/menus.xml',
+    ],
+    'assets': {
+        'web.assets_qweb': [
+        ],
+        'web.assets_backend': [
+        ]
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
