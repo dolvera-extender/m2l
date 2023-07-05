db18100c986d9aaa70897f166cd4314215c2a964
IsaacThkbk <ich3869@gmail.com>
2023-07-05 12:49:07 -0600

    Filters finished

--- m2l_inventory_reports/models/partner.py
+++ m2l_inventory_reports/models/partner.py
@@ -8,7 +8,7 @@ _log = logging.getLogger("\n =====[%s]===" % __name__)
 class ResPartnerM2l(models.Model):
     _inherit = "res.partner"
 
-    product_category_ids = fields.Many2many('product.category', string="Categorías permitidas")
+    product_category_ids = fields.Many2many('product.category', string="Categorías de producto permitidas", help="Categorias de producto que el usuario puede visualizar en el reporte M2L")
 
 
 class M2lFilterRecords(models.TransientModel):
@@ -26,11 +26,8 @@ class M2lFilterRecords(models.TransientModel):
 
     def m2l_filter_products(self):
         tree_view = self.env.ref('m2l_inventory_reports.product_product_m2l_report_tree_view', False)
-        _log.info("vISTA ENCONTRADA ::: %s " % tree_view)
         us_cat = self.env.user.partner_id.product_category_ids
-        _log.info("Categorias del usuario :: %s " % us_cat)
         child_categs = self.get_child_product_categories(us_cat) if us_cat else False
-
         if us_cat:
             if child_categs:
                 ac_ids = (us_cat+child_categs).ids
@@ -48,5 +45,33 @@ class M2lFilterRecords(models.TransientModel):
             'target': "current",
             'domain': [('categ_id', 'in', ac_ids)],
             'views': [[tree_view.id, "tree"]],
+            # 'context': {}
+        }
+
+    def m2l_filter_stock_move_line(self):
+        form_view = self.env.ref('m2l_inventory_reports.stock_move_line_m2l_form_view', False)
+        kanban_view = self.env.ref('m2l_inventory_reports.stock_move_line_m2l_kanban_view', False)
+        tree_view = self.env.ref('m2l_inventory_reports.stock_move_line_m2l_tree_view', False)
+
+        us_cat = self.env.user.partner_id.product_category_ids
+        child_categs = self.get_child_product_categories(us_cat) if us_cat else False
+
+        if us_cat:
+            if child_categs:
+                ac_ids = (us_cat + child_categs).ids
+            else:
+                ac_ids = us_cat.ids
+        else:
+            ac_ids = []
+        return {
+            'name': "Movimientos",
+            'view_mode': 'tree,kanban,form',
+            'view_id': False,
+            'view_type': 'form',
+            'res_model': 'stock.move.line',
+            'type': 'ir.actions.act_window',
+            'target': "current",
+            'domain': [('product_id.categ_id', 'in', ac_ids)],
+            'views': [[tree_view.id, "tree"], [kanban_view.id, "kanban"], [form_view.id, "form"]],
             # 'context': {}
         }
