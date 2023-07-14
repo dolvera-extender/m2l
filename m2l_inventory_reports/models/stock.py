ce1515624ed34e39890cd421dfb29ed1fb361448
IsaacThkbk <ich3869@gmail.com>
2023-07-13 19:08:50 -0600

    ADD: extra fields - ASN, no factura, product category  and product on hand

---
+++ m2l_inventory_reports/models/stock.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+import logging
+_log = logging.getLogger("\n =====[%s]===" % __name__)
+
+
+class StockMoveLineM2l(models.Model):
+    _inherit = "stock.move.line"
+
+    product_category_id = fields.Many2one('product.category', string="Categoria del producto", related="product_id.categ_id")
+    pol_invoice_ref = fields.Char(string="Factura", compute="_compute_invoice_ref", store=False)
+    pol_asn_ref = fields.Char(string="ASN", compute="_compute_invoice_ref", store=False)
+
+    def _compute_invoice_ref(self):
+        for rec in self:
+            _log.info(" linea de compra :: %s -->> FACTURA :: %s" % (rec.move_id.purchase_line_id, rec.move_id.purchase_line_id.x_studio_no_factura))
+            if rec.move_id and rec.move_id.purchase_line_id:
+                rec.pol_invoice_ref = rec.move_id.purchase_line_id.x_studio_no_factura
+                rec.pol_asn_ref = rec.move_id.purchase_line_id.x_studio_asn
+            else:
+                rec.pol_invoice_ref = ""
+                rec.pol_asn_ref = ""
