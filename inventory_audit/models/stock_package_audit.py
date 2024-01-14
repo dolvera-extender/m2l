df10694683b8fca76341e2f2acfef93193449ab3
IsaacThnkPd1 <isaac@cositasmx.com>
2024-01-13 18:34:16 -0600

    [FIX] inventory location, wrong selection algoritm

--- inventory_audit/models/stock_package_audit.py
+++ inventory_audit/models/stock_package_audit.py
@@ -25,42 +25,33 @@ class StockPackageAudit(models.Model):
         picking_type_id = self.env['stock.picking.type'].search([('barcode','=', "WH-INTERNAL")])
     
         _log.info(" TIPO DE OPERACION ::: %s " % picking_type_id)
+        
+        mlidswop = []
+        for line in self.audit_line_ids.filtered(lambda l: l.location_id != False and l.moved == True):
+            _log.info(" MOVIENDO LINEA %s " % line)
+            for pline in line.package_id.quant_ids:
+                mlidswop.append((0,0, {
+                    'product_id': pline.product_id.id,
+                    'location_id': line.location_id.id,
+                    'location_dest_id': self.location_id.id,
+                    'package_id': line.package_id.id,
+                    'result_package_id': line.package_id.id,
+                    'lot_id': pline.lot_id.id,
+                    'qty_done':pline.quantity,
+                    # 'product_uom_qty': pline.quantity,
+                    'product_uom_id': pline.product_uom_id.id
+                }))
+
         picking = self.env['stock.picking'].create({
             'location_id': self.location_id.location_id.id,
             'location_dest_id': self.location_id.location_id.id,
             'picking_type_id': picking_type_id.id,
+            'move_line_ids_without_package': mlidswop
         })
-
-        for line in self.audit_line_ids.filtered(lambda l: l.location_id != False and l.moved == True):
-            _log.info(" MOVIENDO LINEA %s " % line)
-            # Movimiento por cada paquete
-            package_lines = []
-            for pline in line.package_id.quant_ids:
-                # Una linea de movimiento por cada quant del paquete
-                move_line_data = [(0,0,{
-                    'product_id': pline.product_id.id,
-                    'location_id': line.location_id.id,
-                    'location_dest_id': line.location_id.id,
-                    'package_id': line.package_id.id,
-                    'result_package_id': line.package_id.id,
-                    'lot_id': pline.lot_id.id,
-                    'product_uom_qty': pline.quantity,
-                    'product_uom_id': pline.product_uom_id.id
-                })]
-                move = self.env['stock.move'].create({
-                    'name': 'product1_move',
-                    'location_id': line.package_id.location_id.id,
-                    'location_dest_id': self.location_id.id,
-                    'picking_id': picking.id,
-                    'product_id': pline.product_id.id,
-                    'product_uom': pline.product_uom_id.id,
-                    'product_uom_qty': sum(line.package_id.quant_ids.mapped('quantity')),
-                    # 'move_line_ids': move_line_data
-                })
         self.picking_id = picking.id
         picking.action_confirm()
-        # picking.action_assign()
-        # picking.button_validate()
+        picking.action_assign()
+        picking.button_validate()
 
         # Revisar la asignación de paquetes en las lineas de movimiento. 
 
