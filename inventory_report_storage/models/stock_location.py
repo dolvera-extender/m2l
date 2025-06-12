92d0ffe0583cf4a09249420e3402bd652660c446
Alejandro Garcia <alexgama9817@gmail.com>
2025-06-12 11:16:27 -0600

    ADD:: Capacidad de ubicacion

--- inventory_report_storage/models/stock_location.py
+++ inventory_report_storage/models/stock_location.py
@@ -15,20 +15,21 @@ class StockLocationInherit(models.Model):
 
     @api.depends('location_id', 'total_rack')
     def _compute_quantity_rack(self):
-        quants = self.env['stock.quant'].search([('location_id','in',self.ids),('location_id.usage','=','internal')])
+        quants_package = self.env['stock.quant.package'].search([('location_id','in',self.ids),('location_id.usage','=','internal')])
         for rec in self:
             if rec.usage !='internal':
                 rec.percentage_ocupado = 0
                 rec.qty_rack_free = 0
                 continue
-            quants_loc = quants.filtered_domain([('location_id', '=', rec.id)])
+            quants_loc = quants_package.filtered_domain([('location_id', '=', rec.id)])
             total_rack = rec.total_rack
             percentage_ocupado = 0
             qty_rack_free = 0
-            if quants_loc and total_rack:
-                quantity = sum(quants_loc.mapped('quantity'))
+            quantity = len(quants_loc)
+            qty_rack_free = total_rack - quantity
+            if quantity and total_rack:
                 percentage_ocupado = quantity / total_rack * 100
-                qty_rack_free = total_rack - quantity
+
 
             rec.percentage_ocupado = percentage_ocupado
             rec.qty_rack_free = qty_rack_free
--- inventory_report_storage/models/stock_quant.py
+++ inventory_report_storage/models/stock_quant.py
@@ -18,7 +18,18 @@ class StockQuantInherit(models.Model):
             if rec.location_id and rec.location_id.usage == 'internal':
                 rec.location_id._compute_quantity_rack()
 
+class StockQuantPackageInherit(models.Model):
+    _inherit = "stock.quant.package"
 
+    total_rack = fields.Float(related="location_id.total_rack")
+    percentage_ocupado = fields.Float(related="location_id.percentage_ocupado")
+    qty_rack_free = fields.Float(related="location_id.qty_rack_free")
+
+    @api.constrains('name', 'location_id')
+    def _constrains_quantity_loc(self):
+        for rec in self:
+            if rec.location_id and rec.location_id.usage == 'internal':
+                rec.location_id._compute_quantity_rack()
 
 
 
