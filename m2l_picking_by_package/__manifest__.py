beb55944b0266f3a05e8691a6bfaeb8d067e5937
Alejandro Garcia <alexgama9817@gmail.com>
2026-04-16 14:14:43 -0600

    fix:: Reporte operaciones de recoleccion

---
+++ m2l_picking_by_package/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Picking Report by Package',
+    'version': '17.0.1.0.0',
+    'summary': 'Hereda el reporte de Operaciones de Recolección y muestra las líneas agrupadas por paquete',
+    'author': 'Ing. Alejandro Garcia Magaña',
+    'depends': ['stock'],
+    'data': [
+        'reports/report_picking_by_package.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'license': 'LGPL-3',
+}
